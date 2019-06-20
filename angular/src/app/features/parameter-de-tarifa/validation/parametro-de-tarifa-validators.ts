d325efda7d35a97787d4ae6a068bdecf91db17a9
Manish Rane <manish.rane@capgemini.com>
2019-06-20 17:39:09 +0530

    UI changes error

--- angular/src/app/features/parameter-de-tarifa/validation/parametro-de-tarifa-validators.ts
+++ angular/src/app/features/parameter-de-tarifa/validation/parametro-de-tarifa-validators.ts
@@ -3,8 +3,7 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 export class ParametroDeTarifaValidators {
     static numberShouldGreaterThanZero(control: AbstractControl): ValidationErrors | null {
             if (control && control.value) {
-            const val = String(control.value);
-            return val.charAt(0) === '0'  ? { numberShouldGreaterThanZero: true } : null;
+              return control.value > 0 ? null : { numberShouldGreaterThanZero: true };
             }
     }
 }
